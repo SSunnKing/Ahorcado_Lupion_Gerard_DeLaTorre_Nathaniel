d7974ef270a7b71b0aa7151c3210e877f3f0895e
SSunnKing <gerardlupionroca@gmail.com>
2024-10-23 18:09:38 +0200

    added project

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	char mapa[10][10];
+	char pj = 'x';
+	char palabra;
+	int vidas = 5;
+
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf("")//Mostrar mapa
+				printf("Que acción quieres realizar? \n 1.Moverse Arriba 2.Moverse Abajo 3.Moverse Izquierda 4.Moverse Derecha");
+
+		}
+	}
+}
